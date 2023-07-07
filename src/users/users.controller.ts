8d6e3c3ba0f5e1be28f00331bc81a15f7718fca3
jack <jrfisch95@gmail.com>
2023-07-06 21:00:50 -0400

    add basic methods for users service and users controller. still need to add decorators later. add create-user dto.

---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  findAll() {
+    return;
+  }
+
+  findOne() {
+    return;
+  }
+
+  create() {
+    return;
+  }
+
+  remove() {
+    return;
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  findAll() {
+    return;
+  }
+
+  findOne() {
+    return;
+  }
+
+  create() {
+    return;
+  }
+
+  remove() {
+    return;
+  }
+}
